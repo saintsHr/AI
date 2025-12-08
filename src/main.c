609bd4a30f1f8d24a1c752c02683e7f95e818e76
saintsHr <hiqkrs@gmail.com>
2025-12-08 19:55:30 -0300

    refactor: prepared for mnist images & labels

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "utils.h"
 #include "config.h"
 #include "bmp.h"
 
 int main(int argc, char* argv[]){
-    
+
+// =======================================================================================//
+
     // checks correct usage
-    if (argc != 2) return 1;
+    if (argc < 2 || argc > 3) return 1;
+
+// =======================================================================================//
 
     // Neurons
     float inputNeurons[imageSize];
@@ -25,41 +30,19 @@ int main(int argc, char* argv[]){
     float l2Bias[l2Size];
     float outputBias[10];
 
-    // tryes to open the image
-    FILE* img = fopen(argv[1], "rb");
-    if (!img) return 2;
+// =======================================================================================//
 
-    // .bmp headers
-    BITMAPFILEHEADER fileHeader;
-    BITMAPINFOHEADER infoHeader;
+    if (strcmp(argv[1], "train")){
 
-    // fills headers
-    fread(&fileHeader, sizeof(fileHeader), 1, img);
-    fread(&infoHeader, sizeof(infoHeader), 1, img);
-
-    // checks if file is .bmp
-    if (fileHeader.bfType != 0x4D42) return 3;
-    // checks grayscale
-    if (infoHeader.biBitCount != 8) return 4;
-    // checks correct size
-    if (infoHeader.biWidth != imgW || infoHeader.biHeight != imgH) return 5;
-
-    // jump to pixels position
-    fseek(img, fileHeader.bfOffBits, SEEK_SET);
-
-    uint8_t buffer[imageSize];
-
-    // fills buffer with pixel data (inverted)
-    for (int row = imgH - 1; row >= 0; row--) {
-        fread(buffer + (row * imgW), 1, imgW, img);
+    }
+    if (strcmp(argv[1], "test")){
+        
+    }
+    if (strcmp(argv[1], "file")){
+        
     }
 
-    fclose(img);
-
-    // fills input layer with buffer
-    for (int i = 0; i < imageSize; i++) {
-        inputNeurons[i] = buffer[i] / 255.0f;
-    }
+// =======================================================================================//
 
     // processes input -> layer 1
     for (int i = 0; i < l1Size; i++) {
@@ -80,6 +63,8 @@ int main(int argc, char* argv[]){
     for (int i = 0; i < 10; i++){
         printf("%f\n", outputNeurons[i]);
     }
+
+// =======================================================================================//
 
     return 0;
 }
