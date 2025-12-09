bd77b32ea2289aa2460bb413c55a7d9b18cf0a6a
saintsHr <hiqkrs@gmail.com>
2025-12-08 21:02:56 -0300

    refactor: ready for training, test, and single file input

--- src/main.c
+++ src/main.c
@@ -10,7 +10,21 @@ int main(int argc, char* argv[]){
 // =======================================================================================//
 
     // checks correct usage
-    if (argc < 2 || argc > 3) return 1;
+    if (argc < 2 || argc > 3) {
+        printf(
+            "======================================\n"
+            "Incorrect Usage\n"
+            "Usage: ./ai <train/test/file> [image]\n"
+            "======================================\n"
+            "<train> -> trains the AI\n"
+            "<test> -> test the AI's accuracy\n"
+            "<file> -> feeds the AI a .bmp image\n"
+            "[image] -> (optional) image to feed the AI, only used when arg 1 is 'file'\n"
+            "======================================\n"
+        );
+        return 1;
+    }
+
 
 // =======================================================================================//
 
@@ -32,14 +46,61 @@ int main(int argc, char* argv[]){
 
 // =======================================================================================//
 
-    if (strcmp(argv[1], "train")){
+    if (strcmp(argv[1], "train") == 0 && argc == 2){
 
-    }
-    if (strcmp(argv[1], "test")){
+    } else 
+    if (strcmp(argv[1], "test") == 0 && argc == 2){
         
-    }
-    if (strcmp(argv[1], "file")){
+    } else
+    if (strcmp(argv[1], "file") == 0 && argc == 3){
+
+        // tryes to open the image
+        FILE* img = fopen(argv[1], "rb");
+        if (!img) return 2;
         
+        // .bmp headers
+        BITMAPFILEHEADER fileHeader;
+        BITMAPINFOHEADER infoHeader;
+        
+        // fills headers
+        fread(&fileHeader, sizeof(fileHeader), 1, img);
+        fread(&infoHeader, sizeof(infoHeader), 1, img);
+        
+        // checks if file is .bmp
+        if (fileHeader.bfType != 0x4D42) return 3;
+        // checks grayscale
+        if (infoHeader.biBitCount != 8) return 4;
+        // checks correct size
+        if (infoHeader.biWidth != imgW || infoHeader.biHeight != imgH) return 5;
+        
+        // jump to pixels position
+        fseek(img, fileHeader.bfOffBits, SEEK_SET);
+        uint8_t buffer[imageSize];
+        
+        // fills buffer with pixel data (inverted)
+        for (int row = imgH - 1; row >= 0; row--){
+            fread(buffer + (row * imgW), 1, imgW, img);
+        }
+        fclose(img);
+        
+        // fills input layer with buffer
+        for (int i = 0; i < imageSize; i++){
+            inputNeurons[i] = buffer[i] / 255.0f;
+        }
+        
+    } else {
+        printf(
+            "================================================================\n"
+            "Incorrect Usage\n"
+            "  Usage: ./ai <train/test/file> [image]\n"
+            "================================================================\n"
+            "<train>  ->  trains the AI\n"
+            "<test>   ->  test the AI's accuracy\n"
+            "<file>   ->  feeds the AI a .bmp image\n"
+            "[image]  ->  image to feed the AI, needed when arg 1 is 'file'\n"
+            "================================================================\n"
+        );
+        return 2;
     }
 
 // =======================================================================================//
